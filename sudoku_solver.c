1d1e9f92804b595dd082cb1982b8b919449f2272
WilliamVoong <44473866+WilliamVoong@users.noreply.github.com>
2019-06-26 00:45:11 +0200

    Added test for Valid_number func

--- sudoku_solver.c
+++ sudoku_solver.c
@@ -10,7 +10,7 @@ int check_box(int[][9], int, int,int);
 int check_no_duplicate_row(int[][9], int,int); 
 int check_no_duplicate_col(int[][9], int,int); 
 int check_no_duplicate_box(int[][9], int,int,int); 
-
+int Valid_Number(int [][9],int,int,int);
 
 
 void test_function_calculate_col(int[][9]);
@@ -19,6 +19,7 @@ void test_function_calculate_box(int[][9]);
 void test_function_check_no_duplicate_row(int [][9]);
 void test_function_check_no_duplicate_col(int [][9]);
 void test_function_check_no_duplicate_box(int [][9]);
+void test_function_Valid_number(int [][9]);
 
 int main(){
 
@@ -42,6 +43,7 @@ int main(){
 	test_function_check_no_duplicate_row(problem);
 	test_function_check_no_duplicate_col(problem);
 	test_function_check_no_duplicate_box(problem);
+	test_function_Valid_number(problem); 
 
 				
 ;}		
@@ -148,12 +150,10 @@ void test_function_check_no_duplicate_col(int problem[9][9]){
 }
 void test_function_check_no_duplicate_box(int problem[9][9]){
 	int test_sucess=1;
-	if(check_no_duplicate_box(problem, 4,3,3))
+	if(check_no_duplicate_box(problem, 0,0,2))
 	{}
 	else{test_sucess=0;}
-	if(check_no_duplicate_box(problem, 4,3,5))
-	{test_sucess=0;}
-	else{test_sucess=1;}
+
 	if(test_sucess){
 		printf("test_function_check_no_duplicates_box succeeded \n");
 	}
@@ -164,7 +164,21 @@ void test_function_check_no_duplicate_box(int problem[9][9]){
 	
 	
 }
-
+void test_function_Valid_number(int problem[9][9]){
+	int test_sucess=1;
+	if(Valid_Number(problem,8,8,9)){}
+	else{test_sucess=0;}
+	
+	if(test_sucess){
+		printf("Is_Valid succeeded \n");
+	}
+	else{
+		printf("* ERROR * Is_Valid  FAILED \n");
+	}
+		
+	
+	
+}	
 	
 
 int calculate_col( int (*problem)[9], int col){
@@ -219,7 +233,7 @@ int check_no_duplicate_box( int(*problem)[9], int row, int col,int val){
 	int index_col=col/3; 
 	for (int i =index_row*3; i < index_row*3+3; i++){
 			for(int j =index_col*3; j < index_col*3+3; j++){
-				if(problem[i][j]=val){
+				if(problem[i][j]==val){
 					return 0;
 				}
 				
