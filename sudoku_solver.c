279965ba883af1f78c032b2d1369ace50a048997
WilliamVoong <44473866+WilliamVoong@users.noreply.github.com>
2019-06-25 20:14:12 +0200

    added a code base
    implementing checks, for row col, and for a sudoku box.

--- sudoku_solver.c
+++ sudoku_solver.c
@@ -1,11 +1,19 @@
-#include <std.io> 
+#include <stdio.h> 
 
 
+int calculate_row( int[][9], int);
+int calculate_col( int[][9], int);
+int calculate_box( int[][9], int, int);
+int check_col(int[][9], int); 
+int check_row(int[][9], int); 
+int check_box(int[][9], int, int); 
+void test_function_calculate_col(int[][9]);
 
 
 
 int main(){
-  int[][] problem = { 	{ 0, 0, 4,   0, 0, 0,   0, 6, 7 },
+
+  int problem[9][9]= { 	{ 0, 0, 4,   0, 0, 0,   0, 6, 7 },
                         { 3, 0, 0,   4, 7, 0,   0, 0, 5 },
                         { 1, 5, 0,   8, 2, 0,   0, 0, 3 },
                             
@@ -17,68 +25,81 @@ int main(){
                         { 6, 0, 0,   0, 1, 2,   0, 0, 0 },
                         { 9, 3, 0,   0, 0, 0,   7, 1, 0 } 
 						
-					}
+					};
 
-
-
-					
-					
-		
+	test_function_calculate_col(problem); 
+	
 
 				
-}		
+;}		
 
 
-int calculate_row( int[][] problem, int row){
-	int sum=0: 
+
+void test_function_calculate_col(int problem[9][9]){
+	int test_sucess=1;
+	if (calculate_col(problem, 0)==13){}
+	else if (calculate_col(problem, 5)==7){}
+	else if (calculate_col(problem, 7)==14){}
+	else{test_sucess=0;};
+	
+	if(test_sucess){
+		printf("test_function_calculate_row succeeded");
+	}	
+		
+}
+
+
+
+
+
+int calculate_col( int (*problem)[9], int col){
+	int sum=0; 
 	for (int i =0; i < 9; i++){
-	sum+=problem[i][row]; 
+	sum+=problem[i][col]; 
 	}
 	return sum;	
 	
 }
 
 
-
-
-
-
-int calculate_col( int[][] problem, int row){ 
-	int sum=0: 
+int calculate_row( int(*problem)[9], int row){ 
+	int sum=0; 
 	for (int i =0; i < 9; i++){
-	sum+=problem[i][row]; 
+	sum+=problem[row][i]; 
 	}
 	return sum;	
 }
 
 
-int calculate_box( int[][] problem, int row, int col){
-	int sum=0: 
+int calculate_box( int(*problem)[9], int row, int col){
+	int sum=0; 
 	int index_row=row/3; 
 	int index_col=col/3; 
-	for (int i =0; i < index_col*3; i++){
-			for(int j =0; j < index_col*3; j++){
+	for (int i =0; i < index_col*3+3; i++){
+			for(int j =0; j < index_col*3+3; j++){
 				sum+=problem[i][j]; 
+			}	
 				
 	}
+	return sum;
 }
 
-int check_row(int row){
-	if( row > 45){return 0;}
+int check_row(int(*problem)[9], int row){
+	if( calculate_row(problem,row) > 45){return 0;}
 	else		 {return 1; }
 	
 }
 
 
 
-int check_col(int col){
-	if( row > 45){return 0;}
+int check_col( int(*problem)[9], int col){
+	if( calculate_row(problem,col) > 45){return 0;}
 	else		 {return 1;}
 	
 }			
 
-int check_box(int box){
-	if( box > 45){ return 0;}
+int check_box( int(*problem)[9], int row, int col){
+	if( calculate_box(problem,col,row) > 45){ return 0;}
 	else		 {return 1;}
 	
-}			
+}		
