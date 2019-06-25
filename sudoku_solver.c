bd9de9addd8934ef9757ac45d7ea6041f77820bd
WilliamVoong <44473866+WilliamVoong@users.noreply.github.com>
2019-06-25 21:26:13 +0200

    Added checks for duplicates
    Added checks for duplicates for box,row, and col

--- sudoku_solver.c
+++ sudoku_solver.c
@@ -7,7 +7,11 @@ int calculate_box( int[][9], int, int);
 int check_col(int[][9], int); 
 int check_row(int[][9], int); 
 int check_box(int[][9], int, int); 
+
 void test_function_calculate_col(int[][9]);
+void test_function_calculate_row(int[][9]);
+void test_function_calculate_box(int[][9]);
+
 
 
 
@@ -28,7 +32,9 @@ int main(){
 					};
 
 	test_function_calculate_col(problem); 
-	
+	test_function_calculate_row(problem);
+	test_function_calculate_box(problem); 
+	//printf("%d", calculate_box(problem, 5,3));
 
 				
 ;}		
@@ -43,14 +49,49 @@ void test_function_calculate_col(int problem[9][9]){
 	else{test_sucess=0;};
 	
 	if(test_sucess){
-		printf("test_function_calculate_row succeeded");
-	}	
+		printf("test_function_calculate_col succeeded \n");
+	}
+	else{
+		printf("* ERROR * test_function_calculate_col FAILED ");
+	}
+		
 		
 }
-
-
-
-
+void test_function_calculate_row(int problem[9][9]){
+	int test_sucess=1;
+	if (calculate_row(problem, 0)==17){}
+	else if (calculate_row(problem, 5)==14){}
+	else if (calculate_row(problem, 7)==9){}
+	else{test_sucess=0;};
+	
+	
+	if(test_sucess){
+		printf("test_function_calculate_row succeeded \n");
+	}
+	else{
+		printf("* ERROR * test_function_calculate_box FAILED \n");
+	}
+		
+		
+}
+void test_function_calculate_box(int problem[9][9]){
+	int test_sucess=1;
+	
+	if (calculate_box(problem, 0,4)==21){}
+	else if (calculate_box(problem, 5,3)==6){}
+	else if (calculate_box(problem, 7,1)==27){}
+	else{test_sucess=0;};
+	
+	
+	if(test_sucess){
+		printf("test_function_calculate_box succeeded \n");
+	}
+	else{
+		printf("* ERROR * test_function_calculate_box FAILED \n");
+	}
+		
+		
+}
 
 int calculate_col( int (*problem)[9], int col){
 	int sum=0; 
@@ -60,8 +101,6 @@ int calculate_col( int (*problem)[9], int col){
 	return sum;	
 	
 }
-
-
 int calculate_row( int(*problem)[9], int row){ 
 	int sum=0; 
 	for (int i =0; i < 9; i++){
@@ -69,14 +108,12 @@ int calculate_row( int(*problem)[9], int row){
 	}
 	return sum;	
 }
-
-
-int calculate_box( int(*problem)[9], int row, int col){
+int calculate_box( int(*problem)[9], int row, int col,val){
 	int sum=0; 
 	int index_row=row/3; 
 	int index_col=col/3; 
-	for (int i =0; i < index_col*3+3; i++){
-			for(int j =0; j < index_col*3+3; j++){
+	for (int i =index_row*3; i < index_row*3+3; i++){
+			for(int j =index_col*3; j < index_col*3+3; j++){
 				sum+=problem[i][j]; 
 			}	
 				
@@ -84,13 +121,48 @@ int calculate_box( int(*problem)[9], int row, int col){
 	return sum;
 }
 
+int check_no_duplicate_row(int problem[9][9], int row,int val){
+	for (int i =0; i < 9; i++){
+		if(problem[row][i]==val){
+			return 0;
+		}
+	}
+	return 1;
+}
+
+
+int check_no_duplicate_col(int problem[9][9], int row,int value_to_check){
+	for (int i =0; i < 9; i++){
+		if(problem[i][col]==value_to_check){
+			return 0;
+		}
+	}
+	return 1
+}
+
+int check_no_duplicate_box( int(*problem)[9], int row, int col,int val){
+	int index_row=row/3; 
+	int index_col=col/3; 
+	for (int i =index_row*3; i < index_row*3+3; i++){
+			for(int j =index_col*3; j < index_col*3+3; j++){
+				if(problem[i][j]=val){
+					return 0;
+				}
+				
+				
+			}	
+				
+	}
+	return 1;
+	
+}
+
+
 int check_row(int(*problem)[9], int row){
 	if( calculate_row(problem,row) > 45){return 0;}
 	else		 {return 1; }
 	
 }
-
-
 
 int check_col( int(*problem)[9], int col){
 	if( calculate_row(problem,col) > 45){return 0;}
